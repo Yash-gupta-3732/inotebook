8d2457e7345125a067a5acdb41c305e88693e237
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-09-08 13:31:59 +0530

    Setup routes

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,11 +1,26 @@
-
 import './App.css';
+import {
+  BrowserRouter as Router
+  , Routes
+  , Route
+}
+  from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/About' element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
