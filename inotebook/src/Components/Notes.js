b2d786a839e9b8eb54b119e5181643b318edbb04
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-09-11 22:01:25 +0530

    added key props in Noteitem to uniquely identify it

--- inotebook/src/Components/Notes.js
+++ inotebook/src/Components/Notes.js
@@ -1,14 +1,16 @@
 import noteContext from '../Context/notes/noteContext'
-import React,{ useContext } from 'react'
+import { useContext } from 'react'
 import Noteitem from './Noteitem'
 
 const Notes = () => {
     const Context = useContext(noteContext);
+     // eslint-disable-next-line 
     const {notes,setNotes} = Context;
+
   return (
     <div className='row my-3'>
       {notes.map(note=>{
-          return <Noteitem note={note}/>
+          return <Noteitem key={note._id} note={note}/>
         })}
     </div>
   )
