b58a6aa7781ef1b328d9d7fd2c2930309d77c885
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-09-13 17:59:51 +0530

    added delete note functionality

--- inotebook/src/Components/Noteitem.js
+++ inotebook/src/Components/Noteitem.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import { useContext } from 'react'
+import noteContext from '../Context/notes/noteContext'
 
 const Noteitem = (props) => {
     const { note } = props;
+    const Context = useContext(noteContext);
+    const {deleteNote} = Context;
     return (
         <div className='col-md-3'>
             <div className="card my-3" >
                 <div className="card-body">
+                    <div className="d-flex align-items-center justify-content-between">
                     <h5 className="card-title">{note.title}</h5>
+                    <div>
+                    <i className="bi bi-trash mx-1" style={{color:'red'}} onClick={()=>deleteNote(note._id)}></i>
+                    <i class="bi bi-pencil-square mx-1" style={{color:'blue'}}></i>
+                    </div>
+                    </div>
                     <p className="card-text">{note.description}</p>
+                    
                 </div>
             </div>
         </div>
