d20888d7a74fb9ed8903dc16e8bdf920e377e714
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-09-14 19:02:32 +0530

    added handeclick and onchange function and sending it as a props

--- inotebook/src/Components/Notes.js
+++ inotebook/src/Components/Notes.js
@@ -1,18 +1,43 @@
 import noteContext from '../Context/notes/noteContext'
-import { useContext } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import Noteitem from './Noteitem'
 
+import Modal from './Modal'
+
 const Notes = () => {
-    const Context = useContext(noteContext);
-     // eslint-disable-next-line 
-    const {notes,setNotes} = Context;
+  const Context = useContext(noteContext);
+  const { notes, getNote, editNote } = Context;
+  const [note, setNote] = useState({ id: "", etitle: "", edesc: "", etag: "" })
+  const ref = useRef(null)
+  const refClose = useRef(null)
+  useEffect(() => {
+    getNote();
+    // eslint-disable-next-line
+  }, [])
+  const updateNote = (currentNote) => {
+    ref.current.click();
+    setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
+  }
+  const handleClick = (e) => {
+    console.log("updating the note", note);
+    editNote(note.id, note.etitle, note.edescription, note.etag);
+    refClose.current.click(); //to close the modal programmatically
+
+  }
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
+
+  }
 
   return (
-    <div className='row my-3'>
-      {notes.map(note=>{
-          return <Noteitem key={note._id} note={note}/>
+    <>
+      <Modal ref={ref} note={note} refClose={refClose} handleClick={handleClick} onChange={onChange} />
+      <div className='row my-3'>
+        {notes.map(note => {
+          return <Noteitem key={note._id} note={note} updateNote={updateNote} />
         })}
-    </div>
+      </div>
+    </>
   )
 }
 
