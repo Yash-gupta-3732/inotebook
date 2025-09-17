0ea5752fbe7dc524532e70769fd6d51373c8953c
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-09-17 10:37:07 +0530

    added logic so that notes will be shown only when user is loggedin

--- inotebook/src/Components/Notes.js
+++ inotebook/src/Components/Notes.js
@@ -1,17 +1,26 @@
 import noteContext from '../Context/notes/noteContext'
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState, } from 'react'
 import Noteitem from './Noteitem'
+import emptybox from '../Asset/emptybox.gif'
+import { useNavigate } from 'react-router-dom'
 
 import Modal from './Modal'
 
 const Notes = () => {
+  const navigate = useNavigate();
   const Context = useContext(noteContext);
   const { notes, getNote, editNote } = Context;
   const [note, setNote] = useState({ id: "", etitle: "", edesc: "", etag: "" })
   const ref = useRef(null)
   const refClose = useRef(null)
-  useEffect(() => {
-    getNote();
+      useEffect(() => {
+        if(localStorage.getItem('token')){
+          getNote();
+        }
+        else{
+          navigate("/login")
+        }
+
     // eslint-disable-next-line
   }, [])
   const updateNote = (currentNote) => {
@@ -19,7 +28,6 @@ const Notes = () => {
     setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
   }
   const handleClick = (e) => {
-    console.log("updating the note", note);
     editNote(note.id, note.etitle, note.edescription, note.etag);
     refClose.current.click(); //to close the modal programmatically
 
@@ -33,6 +41,7 @@ const Notes = () => {
     <>
       <Modal ref={ref} note={note} refClose={refClose} handleClick={handleClick} onChange={onChange} />
       <div className='row my-3'>
+        <div className='container d-flex justify-content-center' > {notes.length === 0 && <img src={emptybox} style={{ width: "300px" }} alt="No notes to display" />}</div>
         {notes.map(note => {
           return <Noteitem key={note._id} note={note} updateNote={updateNote} />
         })}
