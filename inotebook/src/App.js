3959b637254cabae4937f4f0d13d9de541f1a2da
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-09-17 10:33:22 +0530

    added usestate for the alert

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -12,21 +12,34 @@ import NoteState from './Context/notes/noteStates';
 import Alert from './Components/Alert';
 import Signup from './Components/Signup';
 import Login from './Components/Login';
+import { useState } from 'react';
 
 
 function App() {
+  const [alert, setalert] = useState(null);
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  }
+ 
+
   return (
     <>
       <NoteState>
         <Router>
-          <Navbar />
-          <Alert message={"Beware You Are Deleting Your Notes"}/>
+          <Navbar /> 
+          {alert && <Alert message={alert.msg} type={alert.type}/>}  
           <div className="container">
             <Routes>
-              <Route exact path='/' element={<Home />} />
+              <Route exact path='/' element={<Home showAlert={showAlert} />} />
               <Route exact path='/About' element={<About />} />
-              <Route exact path='/Login' element={<Login />} />
-              <Route exact path='/Signup' element={<Signup />} />
+              <Route exact path='/Login' element={<Login showAlert={showAlert} />} />
+              <Route exact path='/Signup' element={<Signup showAlert={showAlert}/>} />
             </Routes>
           </div>
         </Router>
