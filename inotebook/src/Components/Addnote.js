3601105978561905c10a4a52f45a667a42012e51
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-12 18:46:37 +0530

    added and using darkmode context ...also removed the modal code with the reusable modal component

--- inotebook/src/Components/Addnote.js
+++ inotebook/src/Components/Addnote.js
@@ -1,45 +1,49 @@
-import React, { useContext,useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import noteContext from '../Context/notes/noteContext'
+import darkmodeContext from '../Context/darkmode/darkmodeContext';
+import '../App.css'
+import Modal from './Modal';
+
 
 
 const Addnote = () => {
-    const Context = useContext(noteContext);
-    const {addNote} = Context;
-    const [note, setNote] = useState({title:"",description:"",tag:""})
-    const handleClick=(e)=>{
-        e.preventDefault();//to prevent reloading of page
-        addNote(note.title,note.description,note.tag);
-        setNote({title:"",description:"",tag:""})
-    }
-    const onChange=(e)=>{
-        setNote({...note,[e.target.name]:e.target.value})
+  const Context = useContext(noteContext);
+  const Contextdarkmode = useContext(darkmodeContext);
+  const { mode } = Contextdarkmode;
+  const { addNote } = Context;
+  const refClose = useRef(null);
+  const [note, setNote] = useState({ title: "", description: "", tag: "" })
+  const handleClick = (e) => {
+    e.preventDefault();//to prevent reloading of page
+    addNote(note.title, note.description, note.tag);
+    setNote({ title: "", description: "", tag: "" })
+    refClose.current.click();
+  }
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
 
-    }
+  }
   return (
-    <div>
-      <div className='container mx-3 my-3'>
-        <h1>Add your notes</h1>
-        <form>
-          <div className="mb-3">
-            <label htmlfor="title" className="form-label">Title</label>
-            <input type="text"   required className="form-control" value={note.title} id="title" name="title" aria-describedby="emailHelp" onChange={onChange}/>
-          </div>
-          <div className="mb-3">
-            <label htmlfor="description" className="form-label">Description</label>
-            <input type="text"  required className="form-control" value={note.description} id="description" name="description" onChange={onChange }/>
-          </div>
-         
-          <div className="mb-3">
-            <label htmlfor="tag" className="form-label">Tag</label>
-            <input type="text" className="form-control" value={note.tag} id="tag" name="tag" onChange={onChange }/>
-          </div>
-         
-          <button disabled={note.title.length<5 || note.description.length<5} type="submit" className="btn btn-primary" onClick={handleClick}>Add note</button>
-        </form>
-        <h1>Your notes</h1>
+    <>
+      <div>
+        <div className={`d-flex align-items-center justify-content-between my-3 rounded py-3 px-4 w-100 ice-effect ${mode === 'light' ? 'addnote-light-mode' : 'addnote-dark-mode'}`}>
+          <h1>Your notes</h1>
+
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addNoteModal">
+            Add Note
+          </button>
+        </div>
+        <Modal
+          id="addNoteModal"                     // <-- unique id
+          refClose={refClose}
+          handleFunction={handleClick}
+          onChange={onChange}
+          note={note}
+          saveText="Add Note"
+        />
       </div>
-    </div>
+    </>
   )
 }
 
-export default Addnote
+export default Addnote 
