32b5fff92488ea4645649bf92921b5315cf6e193
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-12 18:59:28 +0530

    added App.css for the ice-effect + darkmode .also darkmodecontext and made some more enhancement

--- inotebook/src/Components/Login.js
+++ inotebook/src/Components/Login.js
@@ -1,48 +1,65 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useContext } from 'react';
+import darkmodeContext from '../Context/darkmode/darkmodeContext';
+
 const Login = (props) => {
     let navigate = useNavigate();
-    const {showAlert} = props;
-    const handleSubmit= async (e)=>{
+    const { showAlert } = props;
+    const Contextdarkmode = useContext(darkmodeContext);
+    const { mode } = Contextdarkmode;
+    const [credentials, setCredentials] = useState({ email: "", password: "" });
+    // clear inputs when login page loads
+    useEffect(() => {
+        setCredentials({ email: "", password: "" });
+    }, []);
+
+    const handleSubmit = async (e) => {
         const host = "http://localhost:5000";
         e.preventDefault();
-         // API CALLS
+        // API CALLS
         const response = await fetch(`${host}/api/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-             body: JSON.stringify({ email: e.target.email.value, password: e.target.password.value }),
+            body: JSON.stringify({ email: credentials.email, password: credentials.password }),
 
         });
-    
+
         const json = await response.json();
-        console.log('LOGIN' ,json) // get auth-token in response
-        if(json.success){
+        console.log('LOGIN', json) // get auth-token in response
+        if (json.success) {
             //save the auth-token and redirect
-            localStorage.setItem('token',json.authtoken);
-            showAlert("Loggedin Successfully","success")
+            localStorage.setItem('token', json.authtoken);
+            showAlert("Loggedin Successfully", "success")
+
             navigate("/") //to redirect to home page
         }
-        else{   
-            showAlert("Invalid credential","danger");
+        else {
+            showAlert("Invalid credential", "danger");
+            setCredentials({ ...credentials, password: "" });
         }
 
+
     }
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    };
     return (
-        <div className='container'> 
+        <div  className={`container rounded ice-effect py-3 px-4 mt-5 pt-2 ${ mode==='light'?'login-light-mode' : 'login-dark-mode'} `} style={{position:'relative',top:'30px'}}>
             <h2 className='my-3'>Login to continue to iNotebook</h2>
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label htmlfor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" name="email" id="email1" aria-describedby="emailHelp" />
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" name="email" id="email1" aria-describedby="emailHelp" onChange={onChange} value={credentials.email} autoComplete="off" />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <label htmlfor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" name="password" id="password" />
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" name="password" id="password" onChange={onChange} value={credentials.password} autoComplete="new-password" />
                 </div>
-    
+
                 <button type="submit" className="btn btn-primary" >Submit</button>
             </form>
         </div>
--- inotebook/src/Components/Signup.js
+++ inotebook/src/Components/Signup.js
@@ -1,7 +1,15 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useContext } from 'react';
+import darkmodeContext from '../Context/darkmode/darkmodeContext';
+import '../App.css'
+import BorderSVG from "../Asset/border.svg";
 const Signup = (props) => {
     let navigate = useNavigate();
+    const Contextdarkmode = useContext(darkmodeContext);
+    const { mode } = Contextdarkmode;
     const { showAlert } = props;
+    const [credentials, setCredentials] = useState({ name: "", email: "", password: "" });
     const handleSubmit = async (e) => {
         const host = "http://localhost:5000";
         e.preventDefault();
@@ -11,7 +19,7 @@ const Signup = (props) => {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ name: e.target.name.value, email: e.target.email.value, password: e.target.password.value, }),
+            body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, }),
 
         });
 
@@ -22,34 +30,43 @@ const Signup = (props) => {
             //save the auth-token and redirect
             localStorage.setItem('token', json.authtoken);
             showAlert("Account Created Successfully", "success")
+
+            // reset input fields BEFORE navigating
+            setCredentials({ name: "", email: "", password: "" });
             navigate("/") //to redirect to home page
         }
 
     }
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    };
     return (
-        <div className='container'>
+
+        <div className={`rounded ice-effect py-3 px-4 mt-5 pt-2 ${mode === 'light' ? 'signup-light-mode' : 'signup-dark-mode'} `} style={{ position: 'relative', top: '30px' }} >
             <h2 className='my-3'>Create an account to use iNotebook</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className={`${mode === 'light' ? 'signup-light-mode' : 'signup-dark-mode'}`}>
                 <div className="mb-3">
-                    <label htmlfor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" name="name" id="name" />
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" name="name" id="name" onChange={onChange} value={credentials.name} autoComplete="off" />
                 </div>
                 <div className="mb-3">
-                    <label htmlfor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" />
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" onChange={onChange} value={credentials.email} autoComplete="off" />
                 </div>
                 <div className="mb-3">
-                    <label htmlfor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" name="password" id="password" required minLength={6} />
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" name="password" id="password" required minLength={6} onChange={onChange} value={credentials.password} autoComplete="new-password" />
                 </div>
                 <div className="mb-3">
-                    <label htmlfor="cpassword" className="form-label">Confirm Password</label>
-                    <input type="password" className="form-control" name="cpassword" id="cpassword" required minLength={6} />
+                    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" name="cpassword" id="cpassword" required minLength={6} onChange={onChange} />
                 </div>
 
                 <button type="submit" className="btn btn-primary" >Submit</button>
             </form>
+
         </div>
+
     )
 }
 
