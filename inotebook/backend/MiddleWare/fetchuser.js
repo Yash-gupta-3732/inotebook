a76e8184c0cf7274db62350f06e9d535a62b8c02
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-12 20:09:49 +0530

    added return keyword

--- inotebook/backend/MiddleWare/fetchuser.js
+++ inotebook/backend/MiddleWare/fetchuser.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET=  "HEYLOYASH@123$^$3";
+const JWT_SECRET = "HEYLOYASH@123$^$3";
 
-const fetchuser = (req,res,next)=>{
+const fetchuser = (req, res, next) => {
     //get the user from the jwt token and add Id to req object
     const token = req.header("auth-token");
-    if(!token){
-        res.status(401).send({error:"Please authenticate using a valid token"})
+    console.log("token",token);
+    if (!token) {
+       return res.status(401).send({ error: "Please authenticate using a valid token" })
     }
     try {
-        const data = jwt.verify(token , JWT_SECRET);
-        req.user = data.user;
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data;
         next();
     } catch (error) {
-        res.status(401).send({error:"Please authenticate using a valid token"})
+        return res.status(401).send({ error: "Please authenticate using a valid token" })
     }
 }
-module.exports= fetchuser; 
+module.exports = fetchuser; 
