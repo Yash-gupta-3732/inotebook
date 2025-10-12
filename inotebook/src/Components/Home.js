9c190e04effe6841397a189fb9502fdb8e7ea805
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-12 20:02:10 +0530

    added some css

--- inotebook/src/Components/Home.js
+++ inotebook/src/Components/Home.js
@@ -1,11 +1,13 @@
 import Addnote from './Addnote'
 import Notes from './Notes'
 
+
 const Home = (props) => {
  const {showAlert} = props;
+
   return (
-    <div>
-      <Addnote />
+    <div className='mt-5 pt-4 '>
+      <Addnote  />
       <Notes showAlert={showAlert}/>
     </div>
   )
