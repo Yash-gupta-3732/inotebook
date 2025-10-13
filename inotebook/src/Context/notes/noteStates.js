44130a994b64936f6bee7e12373b917f7f492577
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-13 18:49:16 +0530

    uses the env file for the host

--- inotebook/src/Context/notes/noteStates.js
+++ inotebook/src/Context/notes/noteStates.js
@@ -2,9 +2,8 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000";
+    const host = process.env.REACT_APP_HOST;
     const initialnotes = []
-
     const [notes, setNotes] = useState(initialnotes)
 
     //GET all notes
