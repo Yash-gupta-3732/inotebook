81f98f4bc01f9a2e3e9f1647d1293c3633aa7f78
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-13 13:58:16 +0530

    Update index.js

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 connTOMongo();
 const app = express()
-const port = 5000
+// const port = 5000
 app.use(cors()) // to allow access to backend from frontend
 
 app.use(express.json());
